18fac69cef45762c40069243bd373ca53171d497
KBarrett89 <barrettkl89@hotmail.com>
2021-07-24 17:40:37 +0100

    KLC-16 #initial spring setup

---
+++ src/main/java/com/bae/Lego/LegoCatalogueApplication.java
@@ -0,0 +1,13 @@
+package com.bae.Lego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegoCatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegoCatalogueApplication.class, args);
+	}
+
+}
