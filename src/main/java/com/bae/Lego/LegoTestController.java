b6999ec81a5ee7bdc97fefc45bc3df83a73fd1ea
KBarrett89 <barrettkl89@hotmail.com>
2021-07-27 10:13:41 +0100

    KLC-17 #deleteLegoKit by id, supports user story KLC-7

--- src/main/java/com/bae/Lego/LegoTestController.java
+++ src/main/java/com/bae/Lego/LegoTestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -56,6 +57,13 @@ public class LegoTestController {
 	public ResponseEntity<Lego> replaceLegoKit(@PathVariable int id, @RequestBody Lego newLegoKit) {
 		Lego body = this.service.replaceLegoKit(id, newLegoKit);
 		return new ResponseEntity<Lego>(body, HttpStatus.ACCEPTED);
+
+	}
+
+	@DeleteMapping("/deleteLegoKit/{id}")
+	public ResponseEntity<String> deleteLegoKit(@PathVariable int id) {
+		String body = this.service.deleteLegoKit(id);
+		return new ResponseEntity<String>(body, HttpStatus.NO_CONTENT);
 
 	}
 }
