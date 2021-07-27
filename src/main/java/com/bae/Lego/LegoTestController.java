e387ffca99a88520a4de8d5684d554830d67a04b
KBarrett89 <barrettkl89@hotmail.com>
2021-07-27 10:04:13 +0100

    KLC-17 #initial setup of Lego Test Controller and Create Lego Kit code - supports user story KLC-6

---
+++ src/main/java/com/bae/Lego/LegoTestController.java
@@ -0,0 +1,35 @@
+package com.bae.Lego;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bae.Lego.data.Lego;
+import com.bae.Lego.service.LegoKitService;
+
+@RestController
+@CrossOrigin
+public class LegoTestController {
+
+	private LegoKitService service;
+
+	public LegoTestController(LegoKitService service) {
+		super();
+		this.service = service;
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello, World!";
+	}
+
+	@PostMapping("/createLegoKit")
+	public ResponseEntity<Lego> createLegoKit(@RequestBody Lego LegoKit) {
+		Lego created = this.service.createLegoKit(LegoKit);
+		return new ResponseEntity<>(created, HttpStatus.CREATED);
+	}
+}
