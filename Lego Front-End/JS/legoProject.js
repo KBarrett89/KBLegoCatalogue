94c8a7f692b21b1109dfeba88183817d3e87272d
KBarrett89 <barrettkl89@hotmail.com>
2021-07-29 10:48:29 +0100

    KLC-8 #front-end crud functunality working update

--- Lego Front-End/JS/legoProject.js
+++ Lego Front-End/JS/legoProject.js
@@ -29,70 +29,7 @@ const getAllLegoKits = () => {
         }).catch(err => console.log(err));
 }
 
-
-const renderLego = (legoKit, outputDiv) => {
-    const newLegoKit = document.createElement('form');
-    newLegoKit.classList.add("col");
-    newLegoKit.classList.add("card");
-
-    const seriesName = document.createElement("label");
-    seriesName.innerText = "Series Name:";
-    newLegoKit.appendChild(seriesName);
-    const seriesNameTextBox = document.createElement("input");
-    seriesNameTextBox.type = "text";
-    seriesNameTextBox.value = legoKit.seriesName;
-    newLegoKit.appendChild(seriesNameTextBox);
-
-
-    const kitNumber = document.createElement("label");
-    kitNumber.innerText = `Kit Number:`;
-    newLegoKit.appendChild(kitNumber);
-    const seriesKitNumBox = document.createElement("input");
-    seriesKitNumBox.type = "number";
-    seriesKitNumBox.value = legoKit.kitNumber;
-    newLegoKit.appendChild(seriesKitNumBox);
-
-
-    const kitName = document.createElement("label");
-    kitName.innerText = `Kit Name:`;
-    newLegoKit.appendChild(kitName);
-    const seriesKitNameBox = document.createElement("input");
-    seriesKitNameBox.type = "text";
-    seriesKitNameBox.value = legoKit.kitName;
-    newLegoKit.appendChild(seriesKitNameBox);
-
-
-    const releaseYear = document.createElement("label");
-    releaseYear.innerText = `Release Year:`;
-    newLegoKit.appendChild(releaseYear);
-    const seriesreleaseYearBox = document.createElement("input");
-    seriesreleaseYearBox.type = "number";
-    seriesreleaseYearBox.value = legoKit.releaseYear;
-    newLegoKit.appendChild(seriesreleaseYearBox);
-
-
-    const updateButton = document.createElement("button");
-    updateButton.type = "submit";
-    updateButton.innerText = "UPDATE";
-    updateButton.classList.add("btn", "btn-primary");
-    updateButton.addEventListener('click', (e) => replaceLegoKit(legoKit.id, e));
-    newLegoKit.appendChild(updateButton);
-
-
-    const deleteButton = document.createElement('button');
-    deleteButton.innerText = "DELETE";
-    deleteButton.classList.add("btn", "btn-primary");
-    deleteButton.addEventListener('click', () => deleteLegoKit(legoKit.id));
-    newLegoKit.appendChild(deleteButton);
-
-
-    outputDiv.appendChild(newLegoKit);
-
-    const gap = document.createElement('br');
-    outputDiv.appendChild(gap);
-}
-
-const replaceLegoKit = (id, e) => {
+const replaceLegoKit = (e, id) => {
     e.preventDefault(); // stops the form submitting in the default way
 
     console.log("in here"); //testing these functions are called
@@ -114,6 +51,76 @@ const replaceLegoKit = (id, e) => {
             alert("Successfully Updated Lego Kit!")
         }).catch(err => console.log(err));
 
+}
+
+const renderLego = (legoKit, outputDiv) => {
+    const newLegoKit = document.createElement('form');
+    newLegoKit.addEventListener('submit', (e) => replaceLegoKit(e, legoKit.id));
+    const deleteButton = document.createElement('button');
+    deleteButton.addEventListener('click', () => deleteLegoKit(legoKit.id));
+    const updateButton = document.createElement("button");
+    //updateButton.addEventListener('click', (e) => replaceLegoKit(legoId, e));
+    newLegoKit.classList.add("col");
+    newLegoKit.classList.add("card");
+    console.log(legoKit);
+
+    const seriesName = document.createElement("label");
+    seriesName.innerText = "Series Name:";
+    newLegoKit.appendChild(seriesName);
+    const seriesNameTextBox = document.createElement("input");
+    seriesNameTextBox.name = "seriesName";
+    seriesNameTextBox.type = "text";
+    seriesNameTextBox.value = legoKit.seriesName;
+    newLegoKit.appendChild(seriesNameTextBox);
+
+
+    const kitNumber = document.createElement("label");
+    kitNumber.innerText = `Kit Number:`;
+    newLegoKit.appendChild(kitNumber);
+    const seriesKitNumBox = document.createElement("input");
+    seriesKitNumBox.name = "kitNumber";
+    seriesKitNumBox.type = "number";
+    seriesKitNumBox.value = legoKit.kitNumber;
+    newLegoKit.appendChild(seriesKitNumBox);
+
+
+    const kitName = document.createElement("label");
+    kitName.innerText = `Kit Name:`;
+    newLegoKit.appendChild(kitName);
+    const seriesKitNameBox = document.createElement("input");
+    seriesKitNameBox.name = "kitName";
+    seriesKitNameBox.type = "text";
+    seriesKitNameBox.value = legoKit.kitName;
+    newLegoKit.appendChild(seriesKitNameBox);
+
+
+    const releaseYear = document.createElement("label");
+    releaseYear.innerText = `Release Year:`;
+    newLegoKit.appendChild(releaseYear);
+    const seriesreleaseYearBox = document.createElement("input");
+    seriesreleaseYearBox.name = "releaseYear";
+    seriesreleaseYearBox.type = "number";
+    seriesreleaseYearBox.value = legoKit.releaseYear;
+    newLegoKit.appendChild(seriesreleaseYearBox);
+
+
+
+    updateButton.type = "submit";
+    updateButton.innerText = "UPDATE";
+    updateButton.classList.add("btn", "btn-primary");
+
+    newLegoKit.appendChild(updateButton);
+
+    deleteButton.innerText = "DELETE";
+    deleteButton.classList.add("btn", "btn-primary");
+
+    newLegoKit.appendChild(deleteButton);
+
+
+    outputDiv.appendChild(newLegoKit);
+
+    const gap = document.createElement('br');
+    outputDiv.appendChild(gap);
 }
 
 const deleteLegoKit = id => {
